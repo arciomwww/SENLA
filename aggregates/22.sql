5c514f8a92c258827929b9d84d48bde07de24268
Vyshinsky Artem Sergeevich <115808321+arciomwww@users.noreply.github.com>
2024-08-08 00:23:27 +0300

    Update 22.sql

--- aggregates/22.sql
+++ aggregates/22.sql
@@ -1,7 +1,5 @@
 select 	dategen.date,
-	(
-		-- correlated subquery that, for each day fed into it,
-		-- finds the average revenue for the last 15 days
+	(	
 		select sum(case
 			when memid = 0 then slots * facs.guestcost
 			else slots * membercost
@@ -15,7 +13,6 @@ select 	dategen.date,
 	)/15 as revenue
 	from
 	(
-		-- generates a list of days in august
 		select 	cast(generate_series(timestamp '2012-08-01',
 			'2012-08-31','1 day') as date) as date
 	)  as dategen
