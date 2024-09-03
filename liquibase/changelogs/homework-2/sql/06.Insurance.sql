62802a91cc17ae36d8e241d39fb99ef2664ff0bc
Vyshinsky Artem Sergeevich <115808321+arciomwww@users.noreply.github.com>
2024-09-03 23:11:57 +0300

    add

--- liquibase/changelogs/homework-2/sql/06.Insurance.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Insurance (
-    id UUID PRIMARY KEY,
-    policy_number VARCHAR(255) NOT NULL UNIQUE,
-    provider VARCHAR(255) NOT NULL,
-    coverage_amount DECIMAL(10, 2) NOT NULL
-);
