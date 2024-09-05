0a5d33812bc09f2a971674585f231d87c01f9a63
Vyshinsky Artem Sergeevich <115808321+arciomwww@users.noreply.github.com>
2024-09-05 19:58:44 +0300

    Revert "add"
    This reverts commit 62802a91cc17ae36d8e241d39fb99ef2664ff0bc.

---
+++ joins/3.sql
@@ -0,0 +1,6 @@
+elect distinct recs.firstname as firstname, recs.surname as surname
+	from 
+		cd.members mems
+		inner join cd.members recs
+			on recs.memid = mems.recommendedby
+order by surname, firstname;
